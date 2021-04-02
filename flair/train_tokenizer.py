2b32b60209374b655daeb2aeb8884b396d430da2
qier <wuqiqi0704@gmail.com>
2021-04-02 12:57:41 +0200

    adjust files

--- flair/train_tokenizer.py
+++ flair/train_tokenizer.py
@@ -54,26 +54,27 @@ for sentence in corpus.get_all_sentences():
 print('functions.py : Nr. of distinguish character: ', len(letter_to_ix.keys()))
 #%%
 # 4. initialize tokenizer
+HIDDEN_DIMs = [8,32,64,128,256,512]
+for HIDDEN_DIM in HIDDEN_DIMs:
+    tokenizer: FlairTokenizer = FlairTokenizer(
+        letter_to_ix=letter_to_ix,
+        embedding_dim=256,
+        hidden_dim=HIDDEN_DIM,
+        num_layers=1,
+        use_CSE=False,
+        use_CRF=False,
+    )
 
-tokenizer: FlairTokenizer = FlairTokenizer(
-    letter_to_ix=letter_to_ix,
-    embedding_dim=4096,
-    hidden_dim=128,
-    num_layers=1,
-    use_CSE=True,
-    use_CRF=False,
-)
+    # 5. initialize trainer
+    from flair.trainers import ModelTrainer
 
-# 5. initialize trainer
-from flair.trainers import ModelTrainer
+    trainer: ModelTrainer = ModelTrainer(tokenizer, corpus)
 
-trainer: ModelTrainer = ModelTrainer(tokenizer, corpus)
-
-# 6. train
-trainer.train(
-    "resources/taggers/3_CSE4096",
-    learning_rate=0.1,
-    mini_batch_size=32,
-    max_epochs=20,
-)
+    # 6. train
+    trainer.train(
+        "resources/taggers/1_h%s"%HIDDEN_DIM,
+        learning_rate=0.1,
+        mini_batch_size=32,
+        max_epochs=30,
+    )
 
---
+++ flair/trainer_GL.py
@@ -0,0 +1,96 @@
+#%% BiLSTM model GL
+from flair.data import Corpus
+
+from flair.datasets import SentenceDataset
+from flair.embeddings import token
+from tokenizer_model import FlairTokenizer
+from tokenizer_model import LabeledString
+
+
+LanguageList = [
+    'HEBREW',
+    'ARABIC',
+    'PORTUGUESE',
+    'ITALIAN',
+    'FRENCH',
+    'SPANISH',
+    'GERMAN',
+    'ENGLISH',
+    'RUSSIAN',
+    'FINNISH',
+    'VIETNAMESE',
+    'KOREAN',
+    'CHINESE',
+    'JAPANESE'
+]
+g1 = ['HEBREW','ARABIC']
+g2 = ['PORTUGUESE','ITALIAN','FRENCH','SPANISH','GERMAN','ENGLISH','FINNISH']
+g3 = ['RUSSIAN', 'KOREAN']
+g4 = ['CHINESE','JAPANESE']
+g5 = ['VIETNAMESE']
+GroupList = [g1,g2,g3,g4,g5]
+GroupNameList = ['group%s'%str(i) for i in range(1,6)]
+
+import pickle
+
+# 1. load your data and convert to list of LabeledString
+data_train, data_test, data_dev = {}, {}, {}
+for language in LanguageList:
+    with open('resources/%s_Train.pickle' % language, 'rb') as f1:
+        train = pickle.load(f1)
+    with open('resources/%s_Test.pickle' % language, 'rb') as f2:
+        test = pickle.load(f2)
+    with open('resources/%s_Dev.pickle' % language, 'rb') as f3:
+        dev = pickle.load(f3)
+
+    data_train[language] = [LabeledString(pair[0]).set_label('tokenization', pair[1]) for pair in train]
+    data_test[language] = [LabeledString(pair[0]).set_label('tokenization', pair[1]) for pair in test]
+    data_dev[language] = [LabeledString(pair[0]).set_label('tokenization', pair[1]) for pair in dev]
+
+for n,g in enumerate(GroupList):
+    data_train['group%s'%str(n+1)]=[] 
+    data_dev['group%s'%str(n+1)]=[]
+    for language in g:
+        data_train['group%s'%str(n+1)] += data_train[language]
+        data_dev['group%s'%str(n+1)] += data_dev[language]
+        # small sample for testing and debugging
+        # data_train['group%s'%str(n+1)] += data_train[language][0:10]
+        # data_dev['group%s'%str(n+1)] += data_dev[language][0:10]
+
+#%%
+# 2. make a Corpus object
+# for language in LanguageList:
+#     corpus: Corpus = Corpus(SentenceDataset(data_train[language]), SentenceDataset(data_test[language]), SentenceDataset(data_dev[language]))
+#     # corpus = corpus.downsample(0.01)
+#     # 3. make the letter dictionary from the corpus
+#     letter_to_ix = {}
+#     letter_to_ix[''] = 0  # need this for padding
+
+#     for sentence in corpus.get_all_sentences():
+#         for letter in sentence.string:
+#             if letter not in letter_to_ix:
+#                 letter_to_ix[letter] = len(letter_to_ix)
+#     print('functions.py : Nr. of distinguish character: ', len(letter_to_ix.keys()))
+
+#     # 4. initialize tokenizer
+#     tokenizer: FlairTokenizer = FlairTokenizer(
+#         letter_to_ix=letter_to_ix,
+#         embedding_dim=256,
+#         hidden_dim=128,
+#         num_layers=1,
+#         use_CSE=False,
+#         use_CRF=False,
+#     )
+
+#     # 5. initialize trainer
+#     from flair.trainers import ModelTrainer
+
+#     trainer: ModelTrainer = ModelTrainer(tokenizer, corpus)
+
+#     # 6. train
+#     trainer.train(
+#         "resources/taggers/3_%s"%language,
+#         learning_rate=0.1,
+#         mini_batch_size=32,
+#         max_epochs=30,
+#     )
