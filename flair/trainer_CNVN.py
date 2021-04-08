80cccf541a8d02bc1e1628533da536ad14ddb2a2
qier <wuqiqi0704@gmail.com>
2021-04-08 16:52:49 +0200

    adjust epoch

--- flair/trainer_CNVN.py
+++ flair/trainer_CNVN.py
@@ -74,6 +74,6 @@ for language in LanguageList:
     trainer.train(
         "resources/taggers/5_CRFCSE_4096%s"%language,
         learning_rate=0.1,
-        mini_batch_size=16,# reduce batch_size and epoch due to GPU Runtime error 
-        max_epochs=15,
+        mini_batch_size=1,# reduce batch_size and epoch due to GPU Runtime error 
+        max_epochs=10,
     )
